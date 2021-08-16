2732c055cdc6620d48dea840d7a2c52d240c68e9
soca-git <soca.mailbox@gmail.com>
2021-08-16 14:16:27 +0100

    go to initial state of project, server working

--- App_Start/Startup.cs
+++ App_Start/Startup.cs
@@ -1,20 +1,13 @@
 ﻿using System;
-using System.Security.Principal;
 using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
 using System.Web.Http.Routing;
 using ExploreCalifornia.Config;
-using ExploreCalifornia.Constraints;
-using ExploreCalifornia.ExceptionHandlers;
-using ExploreCalifornia.Filters;
-using ExploreCalifornia.Loggers;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.Jwt;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Owin;
-using Swashbuckle.Application;
 
 [assembly: OwinStartup(typeof(ExploreCalifornia.Startup))]
 namespace ExploreCalifornia
@@ -26,41 +19,12 @@ namespace ExploreCalifornia
         public void Configuration(IAppBuilder app)
         {
             var config = Startup.HttpConfiguration;
-
-            var json = config.Formatters.JsonFormatter.SerializerSettings;
-            json.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-
-            ConfigureJwt(app);
             ConfigureWebApi(app, config);
-            ConfigureSwashbuckle(config);
         }
 
-        private void ConfigureSwashbuckle(HttpConfiguration config)
-        {
-            config
-                .EnableSwagger(c =>
-                {
-                    c.SingleApiVersion("v1", "A title for your API");
-                    var xmlDocPath = $"{AppDomain.CurrentDomain.BaseDirectory}\\bin\\ExploreCalifornia.xml";
-                    c.IncludeXmlComments(xmlDocPath);
-                })
-                .EnableSwaggerUi();
-        }
+        
         private static void ConfigureWebApi(IAppBuilder app, HttpConfiguration config)
         {
-            var constraintResolver = new DefaultInlineConstraintResolver();
-            constraintResolver.ConstraintMap.Add("identity", typeof(IdConstraint));
-            config.MapHttpAttributeRoutes(constraintResolver);
-            
-            config.Services.Replace(typeof(IExceptionLogger), new UnhandledExceptionLogger());
-            config.Services.Replace(typeof(IExceptionHandler), new UnhandledExceptionHandler());
-            
-            config.MessageHandlers.Add(new TokenValidationHandler());
-
-            config.Filters.Add(new DbUpdateExceptionFilterAttribute());
-            
-            config.Formatters.XmlFormatter.UseXmlSerializer = true;
-
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
 
             config.Routes.MapHttpRoute(
@@ -70,20 +34,6 @@ namespace ExploreCalifornia
             );
 
             app.UseWebApi(config);
-        }
-
-        public void ConfigureJwt(IAppBuilder app)
-        {
-            app.UseJwtBearerAuthentication(
-                new JwtBearerAuthenticationOptions
-                {
-                    AuthenticationMode = AuthenticationMode.Active,
-                    AllowedAudiences = new[] { GlobalConfig.Audience },
-                    IssuerSecurityKeyProviders = new IIssuerSecurityKeyProvider[]
-                    {
-                        new SymmetricKeyIssuerSecurityKeyProvider(GlobalConfig.Issuer, GlobalConfig.Secret)
-                    }
-                });
         }
     }
 }
--- Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ExploreCalifornia.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
